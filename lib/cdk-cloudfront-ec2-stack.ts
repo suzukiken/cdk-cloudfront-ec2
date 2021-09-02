3c070a1a14728a267a81f1bc20d7de5d70283cbc
suzukiken <suzukiken@gmail.com>
2021-09-02 23:08:25 +0000

    first

--- lib/cdk-cloudfront-ec2-stack.ts
+++ lib/cdk-cloudfront-ec2-stack.ts
@@ -1,9 +1,40 @@
 import * as cdk from '@aws-cdk/core';
+import * as acm from "@aws-cdk/aws-certificatemanager";
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as route53 from '@aws-cdk/aws-route53';
+import * as targets from "@aws-cdk/aws-route53-targets/lib";
 
 export class CdkCloudfrontEc2Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const domain = this.node.tryGetContext('domain')
+    const subdomain = this.node.tryGetContext('subdomain')
+    const acmarn = cdk.Fn.importValue(this.node.tryGetContext('useast1_acmarn_exportname'))
+    const http_origin_host = this.node.tryGetContext('http_origin_host')
+
+    const certificate = acm.Certificate.fromCertificateArn(this, 'certificate', acmarn)
+    
+    const distribution = new cloudfront.Distribution(this, 'distribution', {
+      defaultBehavior: { 
+        origin: new origins.HttpOrigin(http_origin_host)
+      },
+      domainNames: [cdk.Fn.join(".", [subdomain, domain])],
+      certificate: certificate,
+    })
+    
+    const zone = route53.HostedZone.fromLookup(this, "zone", {
+      domainName: domain,
+    })
+    
+    const record = new route53.ARecord(this, "record", {
+      recordName: subdomain,
+      target: route53.RecordTarget.fromAlias(
+        new targets.CloudFrontTarget(distribution)
+      ),
+      zone: zone,
+    })
+
   }
 }
