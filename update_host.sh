2961c4ea131b0bfcf714abc0ca1a5fd4e0f11ca1
suzukiken <suzukiken@gmail.com>
2021-10-03 00:48:53 +0000

    update

--- update_host.sh
+++ update_host.sh
@@ -1,6 +1,8 @@
 
 host=$(curl -X GET curl http://169.254.169.254/latest/meta-data/public-hostname)
 
-sed -i -e 's/ec2-[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}.ap-northeast-1.compute.amazonaws.com/'"$host"'/g' ./cdk.json
+sed -i -e 's/ec2-[-0-9]*.ap-northeast-1.compute.amazonaws.com/'"$host"'/' cdk.json
+
+echo $host
 
 cdk deploy
